f09a66c49de2754fe608ba812aaf879188005a36
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-04 18:36:48 +0700

    finish creating chart

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Charts\HasilProduksiChart;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    public function index()
+    public function index(HasilProduksiChart $hasilProduksiChart)
     {
-        return view('dashboard.report.laporanProduksi.index');
+        // $dailyProduct = Product::select('tanggal', DB::raw('SUM(qty_total) as dailyTotal'))
+        //     ->groupBy('tanggal')
+        //     ->get();
+
+        // foreach ($dailyProduct as $data) {
+        //     $tanggal = $data['tanggal'];
+        //     $total = $data['dailyTotal'];
+        //     return $tanggal .  $total;
+        // }
+        // return $dailyProduct[0]['tanggal'];
+
+        return view('dashboard.report.laporanProduksi.index', [
+            "hasilProduksiChart" => $hasilProduksiChart->build()
+        ]);
     }
 }
--- resources/views/dashboard/report/laporanProduksi/index.blade.php
+++ resources/views/dashboard/report/laporanProduksi/index.blade.php
@@ -5,5 +5,13 @@
         <h2 class="text-2xl mb-6">
             LAPORAN PRODUKSI
         </h2>
+
+        <div class="p-6 m-20 bg-white rounded shadow">
+            {!! $hasilProduksiChart->container() !!}
+        </div>
     </div>
+
+    <script src="{{ $hasilProduksiChart->cdn() }}"></script>
+
+    {{ $hasilProduksiChart->script() }}
 @endsection
