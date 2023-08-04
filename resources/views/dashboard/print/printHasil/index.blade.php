0e089b838f75f5fe8ff5909eb7aadd0084022c85
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-05 00:21:56 +0700

    print

--- resources/views/dashboard/print/printHasil/index.blade.php
+++ resources/views/dashboard/print/printHasil/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="space-y-5">
-        <h2 class="text-2xl mb-6">
+        <h2 class="text-2xl mb-6 print:hidden">
             PRINT HASIL
         </h2>
 
@@ -10,7 +10,7 @@
         {{-- search --}}
             <form class="w-1/2" action="/printHasil">   
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
+                <div class="relative print:hidden">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -22,7 +22,7 @@
                 </div>
             </form>
 
-            <button type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 text-center ml-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Print</button>
+            <button type="button" id="printButton" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 text-center ml-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 print:hidden">Print</button>
         </div>
 
         {{-- body --}}
@@ -233,4 +233,15 @@
 
         </div>
     </div>
+    <script>
+        printButton = document.getElementById('printButton');
+
+        function cetakHalaman() {
+            window.print();
+        }
+
+        printButton.addEventListener('click', function () {
+            cetakHalaman();
+        })
+    </script>
 @endsection
