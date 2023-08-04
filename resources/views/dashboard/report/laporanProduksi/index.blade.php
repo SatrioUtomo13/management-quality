e6c42e04a2ab2424527503cf964628a4b61b27b2
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-04 07:52:21 +0700

    prepare installation larapex chart

---
+++ resources/views/dashboard/report/laporanProduksi/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="space-y-5">
+        <h2 class="text-2xl mb-6">
+            LAPORAN PRODUKSI
+        </h2>
+    </div>
+@endsection
