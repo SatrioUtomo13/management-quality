e30cbaee4a418c3bbd151b203e23b0ba02bb6073
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-06 00:20:34 +0700

    params seeding

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function index()
     {
         return view('dashboard.product.index', [
-            "products" => Product::all()
+            "products" => Product::latest()->get()
         ]);
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Param;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Crypt;
@@ -28,6 +29,58 @@ class DatabaseSeeder extends Seeder
             "tim" => "Lead",
             "password" => bcrypt('password'),
             "is_admin" => true
+        ]);
+
+        Param::create([
+            "item" => "5001",
+            "resin" => "DPMB",
+            "type" => "kj",
+            "max_rc" => 43,
+            "min_rc" => 41,
+            "max_vc" => 6,
+            "min_vc" => 7,
+            "wind" => "20",
+            "created_at" => fake()->dateTimeBetween('-1 year', 'now'),
+            "updated_at" => now()
+        ]);
+
+        Param::create([
+            "item" => "11001",
+            "resin" => "DMMC",
+            "type" => "kj",
+            "max_rc" => 43,
+            "min_rc" => 41,
+            "max_vc" => 6,
+            "min_vc" => 7,
+            "wind" => "20",
+            "created_at" => fake()->dateTimeBetween('-1 year', 'now'),
+            "updated_at" => now()
+        ]);
+
+        Param::create([
+            "item" => "16001",
+            "resin" => "DPMD",
+            "type" => "kj",
+            "max_rc" => 43,
+            "min_rc" => 41,
+            "max_vc" => 6,
+            "min_vc" => 7,
+            "wind" => "20",
+            "created_at" => fake()->dateTimeBetween('-1 year', 'now'),
+            "updated_at" => now()
+        ]);
+
+        Param::create([
+            "item" => "9010",
+            "resin" => "DMMD",
+            "type" => "kj",
+            "max_rc" => 43,
+            "min_rc" => 41,
+            "max_vc" => 6,
+            "min_vc" => 7,
+            "wind" => "20",
+            "created_at" => fake()->dateTimeBetween('-1 year', 'now'),
+            "updated_at" => now()
         ]);
 
         Product::factory(7)->create();
