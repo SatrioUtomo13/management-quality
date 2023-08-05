e6b95c421eb28c567a3836766159c45137d6e667
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-05 16:22:30 +0700

    clear feature

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,21 +9,8 @@ use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    public function index(HasilProduksiChart $hasilProduksiChart)
+    public function index()
     {
-        // $dailyProduct = Product::select('tanggal', DB::raw('SUM(qty_total) as dailyTotal'))
-        //     ->groupBy('tanggal')
-        //     ->get();
-
-        // foreach ($dailyProduct as $data) {
-        //     $tanggal = $data['tanggal'];
-        //     $total = $data['dailyTotal'];
-        //     return $tanggal .  $total;
-        // }
-        // return $dailyProduct[0]['tanggal'];
-
-        return view('dashboard.report.laporanProduksi.index', [
-            "hasilProduksiChart" => $hasilProduksiChart->build()
-        ]);
+        return view('dashboard.report.laporanProduksi.index');
     }
 }
