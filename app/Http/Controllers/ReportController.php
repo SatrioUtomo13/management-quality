8ac27d06e8357511fa979ad97d2cbe78a3d65efb
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-08-05 20:24:09 +0700

    hasil produksi

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,6 +11,11 @@ class ReportController extends Controller
 {
     public function index()
     {
-        return view('dashboard.report.laporanProduksi.index');
+        $products = Product::select('user_id', 'item', 'lot', 'berat_awal', 'berat_akhir', 'qty_total')->get();
+
+
+        return view('dashboard.report.laporanProduksi.index', [
+            "products" => $products
+        ]);
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
 @include('layouts.sidebar')
 
 <div class="p-4 sm:ml-64">
-  <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+  <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 bg-slate-100">
     @yield('content')
   </div>
 </div>
